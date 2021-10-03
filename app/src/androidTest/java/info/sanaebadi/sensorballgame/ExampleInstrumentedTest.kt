f961f6bec7ca5c525e8417333564094edd4491d2
Sana <sanaebadi97@gmail.com>
2021-10-03 17:10:45 +0330

    Changed PackageName whole Project

--- app/src/androidTest/java/info/sanaebadi/sensorballgame/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/info/sanaebadi/sensorballgame/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.sensorballgame
+package info.sanaebadi.sensorballgame
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.sensorballgame", appContext.packageName)
+        assertEquals("info.sanaebadi.sensorballgame", appContext.packageName)
     }
 }
