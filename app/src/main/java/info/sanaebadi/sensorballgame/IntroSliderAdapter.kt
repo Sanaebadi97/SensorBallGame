fc2a227740a2ec9a6c3d2fb0f490c9aca1353c54
Sana <sanaebadi97@gmail.com>
2021-10-04 18:12:46 +0330

    added uikit successfully.

---
+++ app/src/main/java/info/sanaebadi/sensorballgame/IntroSliderAdapter.kt
@@ -0,0 +1,34 @@
+package info.sanaebadi.sensorballgame
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import info.sanaebadi.sensorballgame.databinding.OnboardingItemBinding
+
+class IntroSliderAdapter (private val introSlides: List<IntroSlide>)
+    : RecyclerView.Adapter<IntroSliderAdapter.IntroSlideViewHolder>(){
+    var onTextPassed: ((textView: TextView) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IntroSlideViewHolder {
+        return IntroSlideViewHolder(
+            OnboardingItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun getItemCount()=introSlides.size
+
+    override fun onBindViewHolder(holder: IntroSlideViewHolder, position: Int) {
+        holder.bind(introSlides[position])
+    }
+
+    inner class IntroSlideViewHolder(private val binding: OnboardingItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(introSlide: IntroSlide) {
+            binding.textTitle.text = introSlide.title
+            binding.textDescription.text = introSlide.description
+            binding.imageSlideIcon.imageAssetsFolder = "images"
+            binding.imageSlideIcon.setAnimation(introSlide.icon)
+            onTextPassed?.invoke(binding.textTitle)
+        }
+    }
+}
