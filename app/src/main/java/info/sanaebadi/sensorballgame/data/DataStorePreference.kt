3b01b3ed631fa852bb982c458446ebf1ac3f9a0a
Sana <sanaebadi97@gmail.com>
2021-10-05 17:38:26 +0330

    fixed logo

--- app/src/main/java/info/sanaebadi/sensorballgame/data/DataStorePreference.kt
+++ app/src/main/java/info/sanaebadi/sensorballgame/data/DataStorePreference.kt
@@ -18,18 +18,8 @@ class DataStorePreference @Inject constructor(private val preferences: DataStore
      fun fetchOnboarding() = preferences.data.map {
              it[preferencesKey<Boolean>(ONBOARD_KEY)] ?: false  }
 
-    suspend fun saveDiet(diet:String){
-        preferences.edit {
-            it[preferencesKey<String>(DIET_KEY)] = diet
-        }
-    }
-
-    fun fetchDiet() = preferences.data.map {
-        it[preferencesKey<String>(DIET_KEY)]?:"ballGame"
-    }
 
     companion object{
        const val ONBOARD_KEY = "onBoard"
-        const val DIET_KEY = "diet"
     }
 }
